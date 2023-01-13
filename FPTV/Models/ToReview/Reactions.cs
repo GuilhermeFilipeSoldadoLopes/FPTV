dfa28579ea398b664d710618c799d91b106a950a
rui <rui.plinio20@estudantes.ips.pt>
2023-01-13 20:18:50 +0000

    reactions e comments

---
+++ FPTV/Models/ToReview/Reactions.cs
@@ -0,0 +1,36 @@
+﻿using FPTV.Models.Authentication;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace FPTV.Models.ToReview
+{
+    public class Reactions
+    {
+        [Key]
+        [Display(Name = "Reactions ID")]
+        public int reactionsId { get; set; }
+
+        [Required]
+        [Display(Name = "Reaction")]
+        public string reaction { get; set; }
+        
+        [Required]
+        [Display(Name = "User Id")]
+        [ForeignKey("User")]
+        public int userId { get; set; }
+
+        [Required]
+        [Display(Name = "Comment ID")]
+        [ForeignKey("Comments")]
+        public int commentId { get; set; }
+
+        [Required]
+        [Display(Name = "User")]
+        public User? user { get; set; }
+
+        [Required]
+        [Display(Name = "Comments")]
+        public Comments? comments { get; set; }
+    }
+}
