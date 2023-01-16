b34a6a8f1d6cc32843592d89515bb6a5a7a06f95
joaoafonso61 <80059366+joaoafonso61@users.noreply.github.com>
2023-01-16 18:19:43 +0000

    tentativa de google autenticator

--- FPTV/Program.cs
+++ FPTV/Program.cs
@@ -3,10 +3,20 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Drawing;
 using System.Reflection.Emit;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+
 
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
 
-// Add services to the container.
+services.AddAuthentication().AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = configuration["Authentication:Google:323000720092-352meoi1hgmc6n2ff7aot27ohrnhkc28.apps.googleusercontent.com"];
+    googleOptions.ClientSecret = configuration["Authentication:Google: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"];
+});
 
 var connectionString = builder.Configuration.GetConnectionString("FPTV_Context");
 builder.Services.AddDbContext<FPTVContext>(options =>
@@ -15,6 +25,7 @@ builder.Services.AddDbContext<FPTVContext>(options =>
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<FPTVContext>();
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
@@ -29,6 +40,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
