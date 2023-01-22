3c27ac9713515cb4f8ed0fcc868ccaebc591aa42
mizuruRL <andrecfdias42@gmail.com>
2023-01-22 18:43:36 +0000

    Initial steam openid integration

--- FPTV/Program.cs
+++ FPTV/Program.cs
@@ -12,7 +12,9 @@ var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 var configuration = builder.Configuration;
 
-services.AddAuthentication().AddGoogle(googleOptions =>
+services.AddAuthentication()
+    .AddSteam()
+    .AddGoogle(googleOptions =>
 {
     googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
     googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
