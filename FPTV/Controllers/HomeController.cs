b55153884d0e3719d217e5e3b885b1f87c4457a2
GuilhermeFilipeSoldadoLopes <80033323+GuilhermeFilipeSoldadoLopes@users.noreply.github.com>
2023-03-05 23:31:56 +0000

    update

--- FPTV/Controllers/HomeController.cs
+++ FPTV/Controllers/HomeController.cs
@@ -1,21 +1,63 @@
 ﻿using System.Diagnostics;
+using FPTV.Data;
 using FPTV.Models;
+using FPTV.Models.UserModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using PusherServer;
 
 namespace FPTV.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly FPTVContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, FPTVContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+	        var account = _context.Users.ToList().Count();
+
+			var account_txt = (account == 1) ? " user" : " users";
+
+			ViewData["accounts"] = account;
+	        ViewData["account_txt"] = account_txt;
+
+			var visitors = 0;
+
+			if (System.IO.File.Exists("visitors.txt"))
+			{
+				string noOfVisitors = System.IO.File.ReadAllText("visitors.txt");
+				visitors = Int32.Parse(noOfVisitors);
+			}
+
+			++visitors;
+			var visit_text = (visitors == 1) ? " view" : " views";
+
+			System.IO.File.WriteAllText("visitors.txt", visitors.ToString());
+
+			var options = new PusherOptions();
+			options.Cluster = "PUSHER_APP_CLUSTER";
+
+			var pusher = new Pusher(
+				"PUSHER_APP_ID",
+				"PUSHER_APP_KEY",
+				"PUSHER_APP_SECRET", options);
+
+			pusher.TriggerAsync(
+				"general",
+				"newVisit",
+				new { visits = visitors.ToString(), message = visit_text });
+
+			ViewData["visitors"] = visitors;
+			ViewData["visitors_txt"] = visit_text;
+
+			return View();
         }
         public IActionResult Game()
         {
